cd8a80fec8380dd1637a33caf21bde78de831ebd
Tommi351 <to.olufemi1@gmail.com>
2026-06-14 09:39:54 -0600

    Update components/PopularCityList.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/PopularCityList.tsx
+++ components/PopularCityList.tsx
@@ -83,8 +83,8 @@ const data = [
     content: <DummyContent />,
   },
   {
-    category: "India",
-    title: "Harbour Views – Opera House, Bondi Beach & Wildlife",
+    category: "Sydney, Australia",
+    title: "Harbour City – Opera House, Bondi Beach & Wildlife",
     src: "https://images.unsplash.com/photo-1524492412937-b28074a5d7da?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: <DummyContent />,
   },
